ec9b0cb1100f482436dfc991fecd86d72110bc95
marco-perin <30049458+marco-perin@users.noreply.github.com>
2023-04-18 12:04:01 +0200

    Add automatic scan for ST port in get_serial_port

--- src/clab_datalogger_receiver/serial_communication/_utils.py
+++ src/clab_datalogger_receiver/serial_communication/_utils.py
@@ -9,38 +9,48 @@ from serial import Serial
 from serial.tools.list_ports import comports
 
 
-def get_serial_port() -> str | None:
+def get_serial_port(autoscan_st_port: bool = True) -> str | None:
     """Get a serial port.
 
     If more than one is available, \
         a prompt is presented to select which one to use.
 
     """
-    pps = comports()
+    available_ports = comports()
 
-    assert len(pps) > 0, 'No serial ports found!'
+    assert len(available_ports) > 0, 'No serial ports found!'
 
     p_s: int = 0
 
-    if len(pps) > 1:
+    st_port_not_found = True
+
+    if autoscan_st_port and len(available_ports) > 1:
+        for port_idx, port in enumerate(available_ports):
+            if 'STMicroelectronics' in port.description:
+                print('ST port found!')
+                st_port_not_found = False
+                p_s = port_idx
+                break
+
+    if len(available_ports) > 1 and st_port_not_found:
         print('Select serial port')
         print('\n'.join(
             [
                 f' [{i}] | {p.name} | [{p.description}]'
-                for i, p in enumerate(pps)])
+                for i, p in enumerate(available_ports)])
               )
         p_s_in = input()
         p_s = int(p_s_in)
 
     print(
-        f'Connecting to {pps[p_s].name} [{pps[p_s].description}]...',
+        f'Connecting to {available_ports[p_s].name} [{available_ports[p_s].description}]...',
         end=''
     )
     # interface = SerialCommunicationInterface(pps[p_s].name, baudrate)
     print('done')
 
     # return interface
-    return pps[p_s].name
+    return available_ports[p_s].name
 
 
 def get_serial(port, baudrate=115200):
