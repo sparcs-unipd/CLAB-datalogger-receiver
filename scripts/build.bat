fd59bb7537efdbc6aeb46d1c66ca9674aa5558bd
marco-perin <30049458+marco-perin@users.noreply.github.com>
2023-04-26 21:37:26 +0200

    Remove spec file from gitignore

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,6 @@
 
 pyinstaller ^
     --onefile --windowed ^
+    --noconfirm ^
     --add-data "src\clab_datalogger_receiver\icons\SPARCS_logo_v2_nobackground.png;icons" ^
-    .\qt_app.py ^
-    
+    .\clab_datalogger_receiver_app.py    
