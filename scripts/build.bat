652e45a44026bbaadcd0ad1477d9c6186b7862e0
marco-perin <30049458+marco-perin@users.noreply.github.com>
2023-04-26 21:27:30 +0200

    Test different build settings

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,2 +1,6 @@
 
-pyinstaller .\qt_app.py --onefile --windowed --noconfirm
+pyinstaller ^
+    --onefile --windowed ^
+    --add-data "src\clab_datalogger_receiver\icons\SPARCS_logo_v2_nobackground.png;icons" ^
+    .\qt_app.py ^
+    
