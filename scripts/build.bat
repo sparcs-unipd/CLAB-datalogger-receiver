04acd7a72c3d0606760a9b87803909accefeceeb
marco-perin <30049458+marco-perin@users.noreply.github.com>
2023-05-02 13:40:52 +0200

    Fix build script

--- scripts/build.bat
+++ scripts/build.bat
@@ -13,5 +13,6 @@ python -m nuitka ^
     --product-name=clab_datalogger_receiver ^
     --file-version=0.2.3 ^
     --product-version=0.2.3 ^
+    --assume-yes-for-downloads ^ @REM For files needed for compilation
     %* ^
     .\clab_datalogger_receiver_app.py
