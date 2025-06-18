37766f41e0b78170a43281696b9c383241dcd53e
marco-perin <30049458+marco-perin@users.noreply.github.com>
2025-06-18 14:21:40 +0200

    Slightly improve formatting

--- src/clab_datalogger_receiver/qt_app_main.py
+++ src/clab_datalogger_receiver/qt_app_main.py
@@ -14,6 +14,7 @@ import qdarktheme
 from pyqtgraph import (
     GraphicsLayoutWidget as pg_GraphicsLayoutWidget,
     PlotItem,
+    ViewBox,
 )
 from numpy import array as np_array
 from numpy import concatenate as np_concatenate
